97ca4e419e8dd1d43890e7b1749bc89505a0f630
Aleksander Storum <alesto95654@stud.noroff.no>
2023-03-25 12:41:23 +0100

    newsletter feedback finished

--- js/components/newsletter.js
+++ js/components/newsletter.js
@@ -1,12 +1,15 @@
 const newsletterForm = document.querySelector("#newsletter-form");
 const newsletterEmail = document.querySelector("#newsletter-email");
 const newsletterError = document.querySelector("#newsletter-error");
+const newsletterFeedback = document.querySelector("#newsletter-feedback");
 
 function validateForm(event) {
     event.preventDefault();
 
     if (validateEmail(newsletterEmail.value) === true) {
         newsletterError.style.display = "none";
+        newsletterForm.style.display = "none";
+        newsletterFeedback.style.display = "grid";
     } else {
         newsletterError.style.display = "block";
     }
